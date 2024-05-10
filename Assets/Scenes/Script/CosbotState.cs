8f9bf68dbe2f19a90e84427612354f3f54c3a901
ultr42 <ultr42@skiff.com>
2024-05-10 21:28:23 +0800

    added comments

--- Assets/Scenes/Script/CosbotState.cs
+++ Assets/Scenes/Script/CosbotState.cs
@@ -1,5 +1,7 @@
-using FSM;
-
+/// <summary>
+/// Same as FSM.State, but holds a reference to the Bot. So that states do not need to do typecasting.
+/// Otherwise it is mostly a remnant of a previous FSM design structure I had previously.
+/// </summary>
 public class CosbotState : FSM.State
 {
     public Cosbot Bot {get; private set;}
--- Assets/Scenes/Script/FSM/FiniteStateMachine.cs
+++ Assets/Scenes/Script/FSM/FiniteStateMachine.cs
@@ -7,7 +7,15 @@ namespace FSM
     public class FiniteStateMachine : MonoBehaviour
     {
 
+        /// <summary>
+        /// Event callback
+        /// </summary>
+        /// <param name="prev">The previous state</param>
+        /// <param name="next">The next state</param>
         public delegate void OnStateChanged(State prev, State next);
+        /// <summary>
+        /// This event is invoked when the state is changed.
+        /// </summary>
         public event OnStateChanged StateChanged;
 
         [field: SerializeField]
@@ -16,31 +24,42 @@ namespace FSM
         public Coroutine currentStateCoroutine { get; private set; }
 
 
+        /// <summary>
+        /// Sets the initial state of the FSM. Subsequent calls are ignored!
+        /// </summary>
+        /// <param name="initialState">The initial state</param>
         public void SetInitialState(State initialState)
         {
             if (initialState == null) throw new NullReferenceException("The initial state cannot be null!");
+            // Only allow this method to set the initial state once.
             if (currentState == null)
             {
                 Transition(initialState);
+            }
+            else{
+                Debug.LogWarning("Initial state already set! Calls to the method are ignored after the initial call!");
             }
         }
 
         /// <summary>
         /// Transitions to another state. Note that calling this will immediately stop the current state
         /// </summary>
-        /// <param name="nextState"></param>
-        /// <exception cref="NullReferenceException"></exception>
+        /// <param name="nextState">The next state</param>
+        /// <exception cref="NullReferenceException">Do not attempt to set state to null!</exception>
         public void Transition(State nextState)
         {
             if (nextState == null) throw new NullReferenceException("Attempted to transition to null state!");
 
-            
             var prev = currentState;
+            // Immediately stop current state coroutine (To break out of it)
             if (currentStateCoroutine !=null) StopCoroutine(currentStateCoroutine);
+            // Null coalescing operator still needed because initial state is null.
             currentState?.Exit();
             currentState = nextState;
             currentState.Enter();
+            // Invoke event for side-effects.
             StateChanged?.Invoke(prev, nextState);
+            // Start coroutine for current state
             currentStateCoroutine = StartCoroutine(currentState.Start());
         }
 
